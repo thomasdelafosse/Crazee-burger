8d71c2af2f89f43148fd0405b017b034afd38cf8
Thomas Delafosse <bonjour@thomasdelafosse.com>
2023-12-30 23:22:00 +0100

    💄 F03: LOGIN > LoginForm > button > Add styling

--- src/Components/pages/login/LoginForm.jsx
+++ src/Components/pages/login/LoginForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { BsPersonCircle } from "react-icons/bs";
+import { IoChevronForward } from "react-icons/io5";
 
 
 export default function LoginForm() {
@@ -36,63 +37,113 @@ export default function LoginForm() {
             />
             </div>
             <Link to="/order"></Link>
-            <button>Accéder à mon espace </button>
+            <button className="button-with-icon">
+              <span>Accéder à mon espace </span>
+              <IoChevronForward className="icon"/>
+            </button>
         </LoginFormStyled>
       </>
   )
 }
 
 const LoginFormStyled = styled.div`
-background: green;
-text-align: center;
-max-width: 500px;
-min-width: 400px;
-margin: 0px auto;
-padding: 2.5rem 2rem;
-border-radius: 5px;
-font-family: "Amatic SC", cursive;
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0px auto;
+  padding: 2.5rem 2rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
 
-hr {
-  border: 1.5px solid #f56a2c;
-  margin-bottom: 40px;
-} 
+  hr {
+    border: 1.5px solid #f56a2c;
+    margin-bottom: 40px;
+  } 
 
-h1 {
-  color : white;
-  font-size: 48px;
-}
+  h1 {
+    color : white;
+    font-size: 48px;
+  }
 
-h2 {
-  color: #8e8b8b;
-  margin: 20px 10px 10px;
-  color: white;
-  font-size: 36px;
-}
+  h2 {
+    color: #8e8b8b;
+    margin: 20px 10px 10px;
+    color: white;
+    font-size: 36px;
+  }
 
   .input-with-icon {
-  background-color: #fff;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
-  padding: 18px 24px;
-  margin: 18px 0;
+      background-color: #fff;
+      border-radius: 5px;
+      display: flex;
+      align-items: center;
+      padding: 18px 24px;
+      margin: 18px 0;
 
-}
+    .icon{
+      font-size: 15px;
+      margin-right: 8px;
+      color: #93a2b1;
+    }
 
-  .icon{
-  font-size: 15px;
-  margin-right: 8px;
-  color: #93a2b1;
-}
+    input {
+      border: none;
+      font-size: 15px;
+      color: #17161a;
+      width: 100%;
+    }
 
-  input {
-    border: none;
-    font-size: 15px;
-    color: #17161a;
+    &::placeholder {
+      background: white;
+      color: lightgrey;
+    }
   }
 
-  &::placeholder {
-    background: white;
-    color: lightgrey;
+  .button-with-icon{
+    width: 100%;
+    border: 1px solid red;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    white-space: nowrap;
+    text-decoration: none;
+    line-height: 1;
+
+    padding: 18px 24px;
+    border-radius: 5px;
+    font-size: 15px;
+    font-weight: 800;
+    color: white;
+    background-color:  #ff9f1b;
+    border: 1px solid #ff9f1b;
+
+    &:hover:not(:disabled) {
+      background-color: white;
+      color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+      transition: all 200ms ease-out;
+    }
+
+    &:active {
+      color: white;
+      background-color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+    }
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 15px;
+      margin-left: 10px;
+      }
   }
 `
+
+  
