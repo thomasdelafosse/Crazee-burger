762f5663d482e011a8f4f42fafd7aa2380dc85fb
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-01-02 22:54:46 +0100

    💄 F03: LoginPage > Adjust Css

--- src/Components/pages/login/LoginPage.jsx
+++ src/Components/pages/login/LoginPage.jsx
@@ -14,23 +14,14 @@ export default function LoginPage() {
 
 const LoginPageStyled = styled.div`
   height: 100vh;
+  width: 100vw;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
-    ::before {
-      content: "";
-      background-image: url("images/burger-background.jpg");
-      background-size: cover;
-      background-position: center;
-      background-blend-mode: darken;
-
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      z-index: -1;
-    }
+  background-image: url("images/burger-background.jpg");
+  background-size: cover;
+  background-position: center;
+  background-blend-mode: darken;
 `
