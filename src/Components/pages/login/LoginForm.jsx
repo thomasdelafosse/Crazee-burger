9af446814c5bd38dd5ef520264eb51087a4a4183
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-01-03 21:16:41 +0100

    ♻️ F03: LOGIN > LoginForm > Create PrimaryButton

--- src/Components/pages/login/LoginForm.jsx
+++ src/Components/pages/login/LoginForm.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { BsPersonCircle } from "react-icons/bs";
 import { IoChevronForward } from "react-icons/io5";
 import TextInput from '../../reusable-ui/TextInput';
+import PrimaryButton from './PrimaryButton';
 
 export default function LoginForm() {
       const [InputValue, setInputValue] = useState('');
@@ -20,27 +21,27 @@ export default function LoginForm() {
       }
       
   return (
-        <LoginFormStyled action ="submit" onSubmit={handleSubmit}>
-          <div><h1>Bienvenue chez nous !</h1>
-          <hr />
-          <h2>Connectez-vous</h2>
-          </div>
-          <div>
-            <TextInput
-              value={InputValue}
-              onChange={handleChange}
-              type="text"
-              placeholder ="Entrez votre prénom"
-              required
-              Icon={<BsPersonCircle className="icon"/>}
-            />
-
-            <button className="button-with-icon">
-              <span>Accéder à mon espace </span>
-              <IoChevronForward className="icon"/>
-            </button>
-          </div> 
-        </LoginFormStyled>
+    <LoginFormStyled action ="submit" onSubmit={handleSubmit}>
+      <div>
+        <h1>Bienvenue chez nous !</h1>
+        <hr />
+        <h2>Connectez-vous</h2>
+      </div>
+      <div>
+        <TextInput
+          value={InputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder ="Entrez votre prénom"
+          required
+          Icon={<BsPersonCircle className="icon"/>}
+        />
+        <PrimaryButton 
+          label={"Accéder à mon espace"}
+          Icon={<IoChevronForward className="icon"/>}
+        />
+      </div> 
+    </LoginFormStyled>
   )
 }
 
@@ -69,52 +70,13 @@ const LoginFormStyled = styled.form`
     color: white;
     font-size: 36px;
   }
-
-  .button-with-icon{
-    width: 100%;
-    border: 1px solid red;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    white-space: nowrap;
-    text-decoration: none;
-    line-height: 1;
-
-    padding: 18px 24px;
-    border-radius: 5px;
-    font-size: 15px;
-    font-weight: 800;
-    color: white;
-    background-color:  #ff9f1b;
-    border: 1px solid #ff9f1b;
-
-    &:hover:not(:disabled) {
-      background-color: white;
-      color: #ff9f1b;
-      border: 1px solid #ff9f1b;
-      transition: all 200ms ease-out;
-    }
-
-    &:active {
-      color: white;
-      background-color: #ff9f1b;
-      border: 1px solid #ff9f1b;
-    }
-
-    &:disabled {
-      opacity: 0.6;
-      cursor: not-allowed;
-    }
-
-    .icon {
+  .icon {
       display: flex;
       justify-content: center;
       align-items: center;
       font-size: 15px;
       margin-left: 10px;
       }
-  }
 `
 
   
