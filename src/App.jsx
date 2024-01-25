8a6610ff1ed82fb38920a21ec9e3a37cf236724b
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-01-25 16:17:37 +0100

    ♻️ F05: Order > Add OrderPage into Main folder + change App.jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./Components/pages/login/LoginPage";
-import OrderPage from "./Components/pages/order/OrderPage";
 import ErrorPage from "./Components/pages/error/ErrorPage";
+import OrderPage from "./Components/pages/order/Main/OrderPage";
 
 function App() {
   return (
