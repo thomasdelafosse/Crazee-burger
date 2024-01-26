33fed93b6f7817ab207806ed6dfdb11f37a3b260
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-01-26 18:17:47 +0100

    💡 F05: MainPage > remove comment

--- src/Components/pages/order/Main/MainPage.jsx
+++ src/Components/pages/order/Main/MainPage.jsx
@@ -12,7 +12,7 @@ export default function MainPage() {
 }
 
 const MainPageStyled = styled.div`
-  flex: 1; // you can also use "height: calc(95vh - 10vh)";
+  flex: 1;
   background: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   border-radius: 0px 0px 15px 15px;
