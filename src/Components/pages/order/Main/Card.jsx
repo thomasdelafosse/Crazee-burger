f93244d3bb5a1a9ae05551ed505afb8214248c41
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-01-26 18:20:06 +0100

    💄 F05: Menu > Card > Add Card box for menu + CSS

---
+++ src/Components/pages/order/Main/Card.jsx
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+import PrimaryButton from "../../../reusable-ui/PrimaryButton";
+import { fakeMenu2 } from "../../../fakeData/fakeMenu";
+import { formatPrice } from "/src/Components/utils/maths";
+
+export default function Card() {
+  const getPrice = fakeMenu2.find((price) => price.id === 1);
+  const priceReduce = formatPrice(getPrice.price);
+
+  return (
+    <CardStyled>
+      <ImgStyled src="/images/burger-bacon-egg.png" alt="burger-bacon-egg" />
+      <InfoCardStyled className="InfoCard">
+        <TitleStyled>Burger Smoke BBQ</TitleStyled>
+        <PriceButtonStyled>
+          {priceReduce}
+          <PrimaryButton
+            label={"Ajouter"}
+            className={"bouton-ajouter-panier"}
+          />
+        </PriceButtonStyled>
+      </InfoCardStyled>
+    </CardStyled>
+  );
+}
+
+const ImgStyled = styled.img`
+  width: 200px;
+  height: 145px;
+  object-fit: contain;
+  margin-top: 50px;
+  margin-left: 20px;
+  margin-right: 20px;
+`;
+
+const CardStyled = styled.div`
+  width: 240px;
+  height: 330px;
+  background: #ffffff;
+  border-radius: 15px 15px 15px 15px;
+  box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+`;
+
+const InfoCardStyled = styled.div`
+  width: 200px;
+  height: 110px;
+  margin-bottom: 10px;
+  margin-left: 20px;
+  margin-right: 20px;
+  padding: 0px 5px 5px 5px;
+`;
+
+const TitleStyled = styled.div`
+  width: 185.57px;
+  height: 46px;
+  font-family: "Amatic SC", sans-serif;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 45.4px;
+`;
+
+const PriceButtonStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #ffa01b;
+  font-weight: 400;
+  line-height: 22px;
+  size: 16px;
+  font-family: "Open-Sans", sans-serif;
+  margin-top: 10px;
+
+  .bouton-ajouter-panier {
+    width: 95px;
+    height: 38px;
+    border: 1px;
+    font-weight: 700;
+    size: 11px;
+    line-height: 12px;
+    align-items: center;
+    border-radius: 5px;
+  }
+`;
--- src/Components/pages/order/Main/Menu.jsx
+++ src/Components/pages/order/Main/Menu.jsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
+import CardStyled from "./Card";
 
 export default function Menu() {
-  return <MenuStyled></MenuStyled>;
+  return (
+    <MenuStyled>
+      <CardStyled />
+    </MenuStyled>
+  );
 }
 
 const MenuStyled = styled.div`
-  background: grey;
-  width: 100%;
-  height: 100%;
+  background: #f5f5f7;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  padding: 50px 50px 150px;
+  grid-row-gap: 60px;
 `;
