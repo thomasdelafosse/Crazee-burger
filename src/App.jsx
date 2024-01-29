5e1b24e0446a905bd63f068309c58a998b807d7d
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-01-29 01:06:33 +0100

    ✨ F06: Add and Import React-Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./Components/pages/login/LoginPage";
 import ErrorPage from "./Components/pages/error/ErrorPage";
 import OrderPage from "./Components/pages/order/OrderPage";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
