965587fdd9d7ea889a996c2f520d649bd9c4fcd9
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-01-30 18:10:50 +0100

    🚧 F07: Admin > AdminPanel > Layout

--- src/Components/pages/order/Main/MainPage.jsx
+++ src/Components/pages/order/Main/MainPage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import { theme } from "../../../../theme";
 import MenuStyled from "./Menu";
+import AdminPanelStyled from "../Admin/AdminPanel";
 
 export default function MainPage() {
   return (
     <MainPageStyled>
       <MenuStyled />
+      <AdminPanelStyled />
     </MainPageStyled>
   );
 }
