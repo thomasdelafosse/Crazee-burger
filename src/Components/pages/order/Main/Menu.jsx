6c2cb9b700600aa5da1932812cfb02d47231dd32
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-02-19 20:23:42 +0100

    💄 F07: Adjust css

--- src/Components/pages/order/Main/Menu.jsx
+++ src/Components/pages/order/Main/Menu.jsx
@@ -27,10 +27,12 @@ export default function Menu() {
 const MenuStyled = styled.div`
   background: ${theme.colors.background_white};
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-row-gap: 60px;
-  justify-items: center;
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   padding: 50px 50px 150px;
+  justify-items: center;
+  box-shadow: ${theme.shadows.strong};
   overflow-y: scroll;
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
 `;
