cdc44500c90d8ad60cd9b7801d51d5a2390474e2
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-03-15 16:18:39 +0100

    💄 F09: EditForm > Add Text style

--- src/Components/pages/order/Admin/AdminPanel/EditForm.jsx
+++ src/Components/pages/order/Admin/AdminPanel/EditForm.jsx
@@ -1,7 +1,25 @@
 import styled from "styled-components";
+import { theme } from "../../../../../theme";
+import { HiCursorClick } from "react-icons/hi";
 
 export default function EditForm() {
-  return <EditFormStyled>EditForm</EditFormStyled>;
+  return (
+    <div>
+      <EditFormStyled>
+        Cliquer sur un produit pour le modifier
+        <HiCursorClick />
+      </EditFormStyled>
+    </div>
+  );
 }
 
-const EditFormStyled = styled.form``;
+const EditFormStyled = styled.form`
+  font-family: "Amatic SC", cursive;
+  width: 400px;
+  color: ${theme.colors.greyBlue};
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 30px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
