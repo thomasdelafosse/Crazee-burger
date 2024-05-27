48f8503597a650058d937576db268d49805b3174
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-05-27 19:46:22 +0200

    💄 F09: ADMIN > EditForm > Adjust css

--- src/Components/pages/order/Admin/AdminPanel/HintMessage.jsx
+++ src/Components/pages/order/Admin/AdminPanel/HintMessage.jsx
@@ -5,19 +5,24 @@ import { theme } from "../../../../../theme";
 export default function HintMessage() {
   return (
     <HintMessageStyled>
-      Cliquer sur un produit pour le modifier
+      <span className="hint">Cliquer sur un produit pour le modifier</span>
       <HiCursorClick />
     </HintMessageStyled>
   );
 }
 
 const HintMessageStyled = styled.div`
-  font-family: "Amatic SC", cursive;
-  width: 400px;
+  display: flex;
+  align-items: center;
+  font-family: ${theme.fonts.family.stylish};
+  font-size: ${theme.fonts.size.P3};
   color: ${theme.colors.greyBlue};
-  font-size: 24px;
-  font-weight: 400;
-  line-height: 30px;
-  letter-spacing: 0em;
-  text-align: left;
+  width: 400px;
+
+  position: relative;
+  top: 50px;
+
+  span {
+    margin-right: 10px;
+  }
 `;
