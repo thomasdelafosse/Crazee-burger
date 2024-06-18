914039e037dd996358ca21c6c827193866c8238a
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-06-18 15:54:59 +0200

    💄 🚧 F10: BASKET > Add Layout, Colors, Rounded Corners and Shadows

--- src/Components/pages/order/Main/Basket.jsx
+++ src/Components/pages/order/Main/Basket.jsx
@@ -1,9 +1,33 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { theme } from "../../../../theme";
 
 export default function Basket() {
-  return <BasketStyled>Basket</BasketStyled>
+  return (
+    <BasketStyled>
+      <div className="head">Head</div>
+      <div className="body">Body</div>
+      <div className="footer">Footer</div>
+    </BasketStyled>
+  );
 }
 
 const BasketStyled = styled.div`
-  background: pink;
-`
+  display: flex;
+  flex-direction: column;
+
+  .head {
+    height: 70px;
+    background: ${theme.colors.background_dark};
+  }
+
+  .body {
+    flex: 1;
+    background: ${theme.colors.background_white};
+    box-shadow: ${theme.shadows.basket};
+  }
+
+  .footer {
+    height: 70px;
+    background: ${theme.colors.background_dark};
+  }
+`;
