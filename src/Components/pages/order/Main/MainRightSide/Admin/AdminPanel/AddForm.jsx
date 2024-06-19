a0f3535070e348bdb6474151084521d88e5dd54b
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-06-19 16:36:38 +0200

    ♻️ F10: BASKET > Add useSuccessMessage and useMenu (custom hooks)

--- src/Components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm.jsx
+++ src/Components/pages/order/Main/MainRightSide/Admin/AdminPanel/AddForm.jsx
@@ -1,52 +1,37 @@
-import React, { useContext, useState } from "react"
-import OrderContext from "../../../../../../../context/OrderContext"
-import { EMPTY_PRODUCT } from "../../../../../../../enums/product"
-import Button from "../../../../../../reusable-ui/Button"
-import Form from "./Form"
-import SubmitMessage from "./SubmitMessage"
+import { useContext } from "react";
+import OrderContext from "../../../../../../../context/OrderContext";
+import { EMPTY_PRODUCT } from "../../../../../../../enums/product";
+import Form from "./Form";
+import { useSuccessMessage } from "../../../../../../../hooks/useSuccessMessage";
+import SubmitButton from "./SubmitButton";
 
 export default function AddForm() {
   // state
-  const { handleAdd, newProduct, setNewProduct } = useContext(OrderContext)
-  const [isSubmitted, setIsSubmitted] = useState(false)
-
+  const { handleAdd, newProduct, setNewProduct } = useContext(OrderContext);
+  const { isSubmitted, displaySuccessMessage } = useSuccessMessage(4000);
   // comportements
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const newProductToAdd = {
       ...newProduct,
       id: crypto.randomUUID(),
-    }
+    };
 
-    handleAdd(newProductToAdd)
-    setNewProduct(EMPTY_PRODUCT)
+    handleAdd(newProductToAdd);
+    setNewProduct(EMPTY_PRODUCT);
 
-    displaySuccessMessage()
-  }
+    displaySuccessMessage();
+  };
 
   const handleChange = (event) => {
-    const { name, value } = event.target
-    setNewProduct({ ...newProduct, [name]: value })
-  }
-
-  const displaySuccessMessage = () => {
-    setIsSubmitted(true)
-    setTimeout(() => {
-      setIsSubmitted(false)
-    }, 2000)
-  }
+    const { name, value } = event.target;
+    setNewProduct({ ...newProduct, [name]: value });
+  };
 
   // affichage
   return (
     <Form product={newProduct} onSubmit={handleSubmit} onChange={handleChange}>
-      <>
-        <Button
-          className="submit-button"
-          label={"Ajouter un nouveau produit au menu"}
-          version="success"
-        />
-        {isSubmitted && <SubmitMessage />}
-      </>
+      <SubmitButton isSubmitted={isSubmitted} />
     </Form>
-  )
+  );
 }
--- src/Components/pages/order/OrderPage.jsx
+++ src/Components/pages/order/OrderPage.jsx
@@ -3,64 +3,19 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 import Main from "./Main/Main";
 import OrderContext from "../../../context/OrderContext";
-import { fakeMenu } from "../../../fakeData/fakeMenu";
 import { EMPTY_PRODUCT } from "../../../enums/product";
-import { deepClone } from "../../utils/array";
 import NavBar from "./Navbar/NavBar";
+import { useMenu } from "../../../hooks/useMenu";
 
 export default function OrderPage() {
   // state
   const [isModeAdmin, setIsModeAdmin] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [currentTabSelected, setCurrentTabSelected] = useState("add");
-  const [menu, setMenu] = useState(fakeMenu.MEDIUM);
   const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
   const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
   const titleEditRef = useRef();
-
-  // comportements (gestionnaire de state ou "state handlers")
-  const handleAdd = (newProduct) => {
-    // 1. copie du tableau
-    const menuCopy = deepClone(menu);
-
-    // 2. manip de la copie du tableau
-    const menuUpdated = [newProduct, ...menuCopy];
-
-    // 3. update du state
-    setMenu(menuUpdated);
-  };
-
-  const handleDelete = (idOfProductToDelete) => {
-    //1. copy du state
-    const menuCopy = deepClone(menu);
-
-    //2. manip de la copie state
-    const menuUpdated = menuCopy.filter(
-      (product) => product.id !== idOfProductToDelete,
-    );
-    console.log("menuUpdated: ", menuUpdated);
-
-    //3. update du state
-    setMenu(menuUpdated);
-  };
-
-  const handleEdit = (productBeingEdited) => {
-    // 1. copie du state (deep clone)
-    const menuCopy = deepClone(menu);
-
-    // 2. manip de la copie du state
-    const indexOfProductToEdit = menu.findIndex(
-      (menuProduct) => menuProduct.id === productBeingEdited.id,
-    );
-    menuCopy[indexOfProductToEdit] = productBeingEdited;
-
-    // 3. update du state
-    setMenu(menuCopy);
-  };
-
-  const resetMenu = () => {
-    setMenu(fakeMenu.MEDIUM);
-  };
+  const { menu, handleAdd, handleDelete, handleEdit, resetMenu } = useMenu();
 
   const orderContextValue = {
     isModeAdmin,
