8d7bf921691b21c8c4dadd049d5c01497a8ff444
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-07-08 22:55:06 +0200

    🚧 F14: BASKET > Total > Make both elements visible on props change

--- src/Components/reusable-ui/CasinoEffect.jsx
+++ src/Components/reusable-ui/CasinoEffect.jsx
@@ -1,3 +1,38 @@
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import styled from "styled-components";
+
 export default function CasinoEffect({ count, className }) {
-  return <span className={className}> {count}</span>;
+  return (
+    <TransitionGroup
+      component={CasinoEffectStyled}
+      className="transition-group"
+    >
+      <CSSTransition classNames="count-animated" timeout={5000} key={count}>
+        <span className={className}> {count}</span>
+      </CSSTransition>
+    </TransitionGroup>
+  );
 }
+
+const CasinoEffectStyled = styled.div`
+  // MOUNTING
+  .count-animated-enter {
+    background: green;
+  }
+  .count-animated-enter-active {
+    background: blue;
+    transition: 2s;
+  }
+  .count-animated-enter-done {
+    background: pink;
+  }
+
+  // UNMOUNTING
+  .count-animated-exit {
+    background: orange;
+  }
+  .count-animated-exit-active {
+    background: purple;
+    transition: 2s;
+  }
+`;
