0d88c244316b3aad9a9077edba7442359c364138
Thomas Delafosse <bonjour@thomasdelafosse.com>
2024-07-20 14:50:50 +0200

    🚧 F15: FORM > Add selects (ugly)

--- src/Components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.jsx
+++ src/Components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.jsx
@@ -28,6 +28,14 @@ const Form = React.forwardRef(
               ref={ref && input.name === "title" ? ref : null}
             />
           ))}
+          <select name="isAvailable" className="is-available" id="3">
+            <option value={true}>En stock</option>
+            <option value={false}>En rupture</option>
+          </select>
+          <select name="isPublicised" className="is-publicised" id="4">
+            <option value={true}>Sans pub</option>
+            <option value={false}>Avec pub</option>
+          </select>
         </div>
         <div className="form-footer">{children}</div>
       </FormStyled>
@@ -55,6 +63,7 @@ const FormStyled = styled.form`
     grid-template-rows: repeat(3, 1fr);
     grid-template-columns: repeat(3, 1fr);
     grid-row-gap: 8px;
+    grid-column-gap: 8px;
 
     .title {
       grid-area: 1/1/2/4;
@@ -63,7 +72,16 @@ const FormStyled = styled.form`
       grid-area: 2/1/3/4;
     }
     .price {
-      grid-area: 3/1/4/2;
+      /* grid-area: 3/1/4/2; */
+    }
+
+    .is-available {
+      background-color: pink;
+      /* grid-area: 3/1/4/2; */
+    }
+    .is-publicised {
+      background-color: yellow;
+      /* grid-area: 3/1/4/2; */
     }
   }
 
